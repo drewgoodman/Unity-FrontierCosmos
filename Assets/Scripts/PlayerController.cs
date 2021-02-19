528398872c0b4e9e07a3ad8510322fb665ab6fa3
rdogm13 <drg6525@yahoo.com>
2021-02-19 01:07:44 -0800

    tooltips and attributes added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("General Setup Settings")]
+    [Tooltip("How fast ship moves around with player input.")]
     [SerializeField] float controlSpeed = 10f;
+    [Tooltip("Max distance the ship can move left or right from center with player input.")]
     [SerializeField] float xRange = 6f;
+    [Tooltip("Max distance the ship can move up or down from center with player input.")]
     [SerializeField] float yRange = 6f;
+
+    [Header("Laser Gun Array")]
+    [Tooltip("Laser gun particle objects")]
     [SerializeField] GameObject[] lasers;
 
+    [Header("Screen Position Settings")]
+    [Tooltip("Rotation for pitch based on ship's vertical screen position.")]
     [SerializeField] float positionPitchFactor = -2f;
-    [SerializeField] float controlPitchFactor = -10f;
+    [Tooltip("Rotation for yaw based on ship's horizontal screen position.")]
     [SerializeField] float positionYawFactor = 2f;
+    [Tooltip("Rotation for pitch based on ship's vertical player input.")]
+    [SerializeField] float controlPitchFactor = -10f;
+    [Tooltip("Rotation for pitch based on ship's horizontal player input.")]
     [SerializeField] float controlRollFactor = -10f;
 
 
