5f1e9e38d9345e3f5f98f4fd5e233c3f8873ed0c
rdogm13 <drg6525@yahoo.com>
2021-02-20 23:20:33 -0800

    terrain expansion

---
+++ Assets/Scripts/EnemyHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHandler : MonoBehaviour
+{
+    void OnParticleCollision(GameObject particle)
+    {
+        Debug.Log($"{this.name} was struck by {particle.gameObject.name}!");
+        Destroy(this.gameObject);
+    }
+}
