a05039c488d6b2349f4c7d563a64a98d8d800bc3
rdogm13 <drg6525@yahoo.com>
2021-02-20 22:39:16 -0800

    collision handling deubg log

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(this.name + "-- crashed into --" + collision.gameObject.name);
+    }
+
+    void OnTriggerEnter(Collider trigger)
+    {
+        Debug.Log(this.name + "** was triggered by **" + trigger.gameObject.name);
+    }
+
+
+}
