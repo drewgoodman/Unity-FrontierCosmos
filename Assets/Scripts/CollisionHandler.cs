a4f38ca4dacda1f825c5837a1872eedc70eca141
rdogm13 <drg6525@yahoo.com>
2021-03-02 15:07:42 -0800

    added reload on death

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("How long before reloading level on collision death?")]
+    float levelLoadDelay = 1f;
 
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(this.name + "-- crashed into --" + collision.gameObject.name);
-    }
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     Debug.Log(this.name + "-- crashed into --" + collision.gameObject.name);
+    //     HandleDeathSequence();
+    // }
 
     void OnTriggerEnter(Collider trigger)
     {
         Debug.Log(this.name + "** was triggered by **" + trigger.gameObject.name);
+        HandleDeathSequence();
     }
 
+    void HandleDeathSequence()
+    {
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
 
 }
