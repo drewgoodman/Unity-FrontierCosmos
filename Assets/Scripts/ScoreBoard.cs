ab5f39f94912367672eb691903e1b5859282b6b3
rdogm13 <drg6525@yahoo.com>
2021-03-02 17:23:55 -0800

    new prefabs / score tied to canvas text

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    int scoreDigitMax = 8;
+    TMP_Text scoreText;
+
+    void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = new string('0', scoreDigitMax);
+    }
 
     public void ModifyScore(int amountToModify)
     {
         score += amountToModify;
-        print("New Score!: " + score);
+        scoreText.text = score.ToString().PadLeft(scoreDigitMax, '0');
     }
 
 }
