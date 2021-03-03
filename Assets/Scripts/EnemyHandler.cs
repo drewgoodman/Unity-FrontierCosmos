e6cb02785518a11075ae168ffc2720cb284b3b78
rdogm13 <drg6525@yahoo.com>
2021-03-02 16:16:55 -0800

    instantiating enemy destruction VFX with automatic cleanup

--- Assets/Scripts/EnemyHandler.cs
+++ Assets/Scripts/EnemyHandler.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class EnemyHandler : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
     void OnParticleCollision(GameObject particle)
     {
-        Debug.Log($"{this.name} was struck by {particle.gameObject.name}!");
+        // Debug.Log($"{this.name} was struck by {particle.gameObject.name}!");
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(this.gameObject);
     }
 }
---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    [SerializeField] float timeTilDestroy = 3f;
+    void Start()
+    {
+        Destroy(gameObject, timeTilDestroy);
+    }
+}
